832e1c0f7d830261911f2b3cfcc7534a657869b5
komarite <ulasgulhan9@gmail.com>
2024-05-02 11:30:47 +0300

    Task model created and added to the admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+
+class TasksAdmin(admin.ModelAdmin):
+    list_display    = ('task', 'is_complete', 'is_active')
+
+
+admin.site.register(Task, TasksAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    task        = models.CharField(max_length=300)
+    is_complete = models.BooleanField(default=False)
+    is_active   = models.BooleanField(default=True)
+
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
